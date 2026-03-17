4fac2c333f99705c25347e28d2dd5645068a05e8
Erik Holum <erik.holum@nasa.gov>
2026-03-17 10:42:18 -0400

    Echo sourcing from the pixi entrypoint

--- scripts/entrypoint_pixi.sh
+++ scripts/entrypoint_pixi.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# I haven't gotten printouts working in pixi, but oh well, this should technically work?
+# Pixi will swallow stdout/stderr, so redirect output straight to the tty
+# so it shows up.
+message() { echo "[pixi] $*" > /dev/tty; }
+
 if [ -f "${PIXI_PROJECT_ROOT}/install/setup.bash" ]; then
-    echo "Sourcing ${PIXI_PROJECT_ROOT}/install/setup.bash"
+    message "Sourcing ${PIXI_PROJECT_ROOT}/install/setup.bash"
     # shellcheck source=/dev/null
     source "${PIXI_PROJECT_ROOT}/install/setup.bash"
 else
-    echo "The ${PIXI_PROJECT_ROOT} workspace is not yet built."
-    echo "To build:"
-    echo "  cd ${PIXI_PROJECT_ROOT}"
-    echo "  colcon build"
+    message "The ${PIXI_PROJECT_ROOT} workspace is not yet built."
+    message "To build:"
+    message "  cd ${PIXI_PROJECT_ROOT}"
+    message "  colcon build"
 fi
