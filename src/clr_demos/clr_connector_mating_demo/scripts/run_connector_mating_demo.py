a0e0b8a2993d637bf7ff4e8ebb62586a3c5dc109
Zarrin Bashir <zarrin.t.bashir@nasa.gov>
2026-07-23 13:31:44 -0500

    Pre-commit + other edits.

--- src/clr_demos/clr_connector_mating_demo/scripts/run_connector_mating_demo.py
+++ src/clr_demos/clr_connector_mating_demo/scripts/run_connector_mating_demo.py
@@ -7,23 +7,19 @@ from ament_index_python.packages import get_package_share_directory
 
 from imetro_behavior.executor import BehaviorTreeExecutor
 
+
 def main(args=None):
     rclpy.init(args=args)
-    
+
     pkg_share = get_package_share_directory("clr_connector_mating_demo")
     config_path = os.path.join(pkg_share, "config", "clr_connector_mating_config.yaml")
 
-    node = Node(
-        "run_behavior",
-        parameter_overrides=[
-            rclpy.parameter.Parameter("behavior_config", value=config_path)
-        ]
-    )
+    node = Node("run_behavior", parameter_overrides=[rclpy.parameter.Parameter("behavior_config", value=config_path)])
 
     executor = MultiThreadedExecutor()
     executor.add_node(node)
 
-    bt_exec = BehaviorTreeExecutor(node)
+    bt_exec = BehaviorTreeExecutor(node)  # noqa
 
     try:
         executor.spin()
@@ -33,6 +29,7 @@ def main(args=None):
         executor.shutdown()
         node.destroy_node()
         rclpy.try_shutdown()
+
 
 if __name__ == "__main__":
     main()
